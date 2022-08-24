9666dfb7b0bcfcec77707a9b449072b3293caa32
Ryan Duffy <ryan@replay.io>
2022-08-24 12:34:57 -0700

    jest

--- tests/example-2.spec.js
+++ tests/example-2.spec.js
@@ -14,3 +14,17 @@ it("basic test", async () => {
   await expect(await title.textContent()).toBe("Playwright");
   await browser.close();
 });
+
+it("basic test 2", async () => {
+  const browser = await playwright.firefox.launch({
+    ...devices["Replay Firefox"].launchOptions,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto("https://playwright.dev/");
+  const title = page.locator(".navbar__inner .navbar__title");
+
+  await expect(await title.textContent()).toBe("Playwright");
+  await browser.close();
+});
